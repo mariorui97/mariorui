99d6c561140c84577e19b6ace170a0eac1010e3a
Mário Rui <marinhocosta@live.com.pt>
2022-01-04 06:32:20 +0000

    more css

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -5,13 +5,13 @@ function Socials() {
     return (
             <div className="social-container">
                 <ul className="social-icons">
-                    <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                    <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i className="fa fa-github"></i></a></li>
+                    <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"><i className="fab fa-github"></i></a></li>
                 </ul>                
                 <ul className="social-icons" id="cv">
-                        <li><a href="#"><i className="fa fa-file"></i></a></li>
+                        <li><a href="#"><i className="far fa-address-card" id="cv-icon"></i></a></li>
                 </ul>
                 
 
