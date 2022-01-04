8375e117d026ecae9297480e68dc044efc86dad7
Mário Rui <marinhocosta@live.com.pt>
2022-01-04 05:54:38 +0000

    css and hover effects

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Socials.css'
+
 function Socials() {
     return (
             <div className="social-container">
@@ -8,11 +9,10 @@ function Socials() {
                     <li><a href="#"><i className="fa fa-twitter"></i></a></li>
                     <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
                     <li><a href="#"><i className="fa fa-github"></i></a></li>
-                </ul>
-                
-                    <ul className="social-icons ">
+                </ul>                
+                <ul className="social-icons" id="cv">
                         <li><a href="#"><i className="fa fa-file"></i></a></li>
-                    </ul>
+                </ul>
                 
 
             </div>        
